449b06cf00e9803c5c5cb2248b909da3d027472e
sa3dalla <ugs.160345@eng.suez.edu.eg>
2023-03-13 12:46:26 +0200

    attendance table

--- graduation-project/src/pages/application/components/Attendace.jsx
+++ graduation-project/src/pages/application/components/Attendace.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../style/attendance.css";
 import ChartDonate from "./Chart-donate";
+import TableDay from "./Table-day";
 export default function Attendance() {
   return (
-    <div>
-      <ChartDonate/>
+    <div className="attendance">
+      <h3>Attendance</h3>
+      <TableDay/>
     </div>
   );
 }
--- graduation-project/src/pages/application/components/Table-day.jsx
+++ graduation-project/src/pages/application/components/Table-day.jsx
@@ -3,7 +3,131 @@ import React from "react";
 export default function TableDay() {
   return (
     <div>
-      <h1>hello sa3dalla</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Deparment</th>
+            <th>status</th>
+            <th>Arrival time</th>
+            <th>Leave Time</th>
+          </tr>
+        </thead>
+        <tbody> 
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+          <tr>
+            <td>خالد الشربيني احمد حسن</td>
+            <td>worhshop</td>
+            <td>present</td>
+            <td>08:23</td>
+            <td>1230</td>
+          </tr>
+        </tbody> 
+      </table>
     </div>
   );
 }
