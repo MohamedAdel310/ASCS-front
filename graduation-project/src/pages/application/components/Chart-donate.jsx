e7ffc1ed1d5f070f8475a0366ec3c949f4e94283
sa3dalla <ugs.160345@eng.suez.edu.eg>
2023-03-13 16:13:51 +0200

    donates make <3

--- graduation-project/src/pages/application/components/Chart-donate.jsx
+++ graduation-project/src/pages/application/components/Chart-donate.jsx
@@ -1,28 +1,94 @@
 import React from "react";
-import { Chart } from "react-google-charts";
-
-const data = [
-  ["Task", "Hours per Day"],
-  ["Work", 18],
-  ["Eat", 2] // CSS-style declaration
-];
-
-const options = {
-  title: "My Daily Activities",
-  pieHole: 0.6,
-  is3D: false,
-};
-
 export default function ChartDonate() {
   return (
-    <div>
-      <Chart
-        chartType="PieChart"
-        width="20%"
-        height="100px"
-        data={data}
-        options={options}
-      />
+    <div className="donate">
+      <div className="container">
+        <h4>Absense</h4>
+        <div className="skills">
+          <div className="outer">
+            <div className="inner">
+              <div id="number">
+                8
+              </div>
+            </div>
+          </div>
+
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+            <defs>
+              <linearGradient id="GradientColor">
+                  <stop offset="0%" stop-color="#e91e63" />
+                  <stop offset="100%" stop-color="#673ab7" />
+              </linearGradient>
+            </defs>
+            <circle className="circle1" cx="70" cy="70" r="60" stroke-linecap="round" />
+          </svg>
+        </div>
+      </div>
+      <div className="container">
+      <h4>Late</h4>
+        <div className="skills">
+          <div className="outer">
+            <div className="inner">
+              <div id="number">
+                29
+              </div>
+            </div>
+          </div>
+
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+            <defs>
+              <linearGradient id="GradientColor">
+                  <stop offset="0%" stop-color="#e91e63" />
+                  <stop offset="100%" stop-color="#673ab7" />
+              </linearGradient>
+            </defs>
+            <circle className="circle2" cx="70" cy="70" r="60" stroke-linecap="round" />
+          </svg>
+        </div>
+      </div>
+      <div className="container">
+      <h4>Undertime</h4>
+        <div className="skills">
+          <div className="outer">
+            <div className="inner">
+              <div id="number">
+                18
+              </div>
+            </div>
+          </div>
+
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+            <defs>
+              <linearGradient id="GradientColor">
+                  <stop offset="0%" stop-color="#e91e63" />
+                  <stop offset="100%" stop-color="#673ab7" />
+              </linearGradient>
+            </defs>
+            <circle className="circle3" cx="70" cy="70" r="60" stroke-linecap="round" />
+          </svg>
+        </div>
+      </div>
+      <div className="container">
+      <h4>Overtime</h4>
+        <div className="skills">
+          <div className="outer">
+            <div className="inner">
+              <div id="number">
+                9
+              </div>
+            </div>
+          </div>
+
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+            <defs>
+              <linearGradient id="GradientColor">
+                  <stop offset="0%" stop-color="#e91e63" />
+                  <stop offset="100%" stop-color="#673ab7" />
+              </linearGradient>
+            </defs>
+            <circle className="circle4" cx="70" cy="70" r="60" stroke-linecap="round" />
+          </svg>
+        </div>
+      </div>
     </div>
-  );
-}
+  )}
