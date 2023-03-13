5631f8a7b9da7480d641eddc28a431da10e367eb
MohamedAdel310 <99290531+MohamedAdel310@users.noreply.github.com>
2023-03-13 18:37:29 +0200

    edit donate chart add props

--- graduation-project/src/pages/application/components/Chart-donate.jsx
+++ graduation-project/src/pages/application/components/Chart-donate.jsx
@@ -1,94 +1,37 @@
 import React from "react";
-export default function ChartDonate() {
+export default function ChartDonate(props) {
   return (
-    <div className="donate">
-      <div className="container">
-        <h4>Absense</h4>
-        <div className="skills">
-          <div className="outer">
-            <div className="inner">
-              <div id="number">
-                8
-              </div>
-            </div>
+    <div className="container">
+      {console.log(props.num)}
+      <h4>Absense</h4>
+      <div className="skills">
+        <div className="outer">
+          <div className="inner">
+            <div id="number">{props.num}</div>
           </div>
-
-          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
-            <defs>
-              <linearGradient id="GradientColor">
-                  <stop offset="0%" stop-color="#e91e63" />
-                  <stop offset="100%" stop-color="#673ab7" />
-              </linearGradient>
-            </defs>
-            <circle className="circle1" cx="70" cy="70" r="60" stroke-linecap="round" />
-          </svg>
         </div>
-      </div>
-      <div className="container">
-      <h4>Late</h4>
-        <div className="skills">
-          <div className="outer">
-            <div className="inner">
-              <div id="number">
-                29
-              </div>
-            </div>
-          </div>
 
-          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
-            <defs>
-              <linearGradient id="GradientColor">
-                  <stop offset="0%" stop-color="#e91e63" />
-                  <stop offset="100%" stop-color="#673ab7" />
-              </linearGradient>
-            </defs>
-            <circle className="circle2" cx="70" cy="70" r="60" stroke-linecap="round" />
-          </svg>
-        </div>
-      </div>
-      <div className="container">
-      <h4>Undertime</h4>
-        <div className="skills">
-          <div className="outer">
-            <div className="inner">
-              <div id="number">
-                18
-              </div>
-            </div>
-          </div>
-
-          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
-            <defs>
-              <linearGradient id="GradientColor">
-                  <stop offset="0%" stop-color="#e91e63" />
-                  <stop offset="100%" stop-color="#673ab7" />
-              </linearGradient>
-            </defs>
-            <circle className="circle3" cx="70" cy="70" r="60" stroke-linecap="round" />
-          </svg>
-        </div>
-      </div>
-      <div className="container">
-      <h4>Overtime</h4>
-        <div className="skills">
-          <div className="outer">
-            <div className="inner">
-              <div id="number">
-                9
-              </div>
-            </div>
-          </div>
-
-          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
-            <defs>
-              <linearGradient id="GradientColor">
-                  <stop offset="0%" stop-color="#e91e63" />
-                  <stop offset="100%" stop-color="#673ab7" />
-              </linearGradient>
-            </defs>
-            <circle className="circle4" cx="70" cy="70" r="60" stroke-linecap="round" />
-          </svg>
-        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          width="160px"
+          height="160px"
+        >
+          <defs>
+            <linearGradient id="GradientColor">
+              <stop offset="0%" stop-color="#e91e63" />
+              <stop offset="100%" stop-color="#673ab7" />
+            </linearGradient>
+          </defs>
+          <circle
+            className="circle1"
+            cx="70"
+            cy="70"
+            r="60"
+            stroke-linecap="round"
+          />
+        </svg>
       </div>
     </div>
-  )}
+  );
+}
