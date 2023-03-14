ff86f55540ab22183c7f7288d5537604b5f4ef54
youssefnasserw <yoyozico2016@gmail.com>
2023-03-14 17:07:29 +0200

    daily report

--- graduation-project/src/pages/application/daily-report/components/Card-Violation.jsx
+++ graduation-project/src/pages/application/daily-report/components/Card-Violation.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import branchLogo from "F:/ASCS-front/graduation-project/src/assets/image/branch.jpg";
 
-export default function CardViolation() {
+export default function CardViolation(props) {
   return (
     <div className="card-violation">
-      <h1 className="card-violation--header">hello from card violation</h1>
-    </div>
+      <div className="card-violation--header">
+      
+      <img id="image" src={branchLogo} alt="branch logo" />
+      
+      <h4 id="text">{props.text}</h4>
+      <div id="number">{props.num}</div>
+      </div>
+      </div>
+
   );
 }
--- graduation-project/src/pages/application/daily-report/components/Status-Violation.jsx
+++ graduation-project/src/pages/application/daily-report/components/Status-Violation.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 
-export default function StatusViolation() {
+export default function StatusViolation(props) {
+
   return (
+ 
     <div className="status-siola">
-      <h1 className="status-violation--header">hello from status violation</h1>
+      <h4 id="status-violation--header">Detected Vehicles</h4>
+      <h3 id="status-violation--info"> Detected vehicle entered the factory at <b id="bold">{props.num}</b> -The plate number is <b id="bold">{props.num2}</b> -Reported camera: <b id="bold">{props.num3}</b></h3>
+      
+
+    
+    <div className="critcal_container">
+
+    <h4 id="critcal_header"> Critcal Level  </h4> 
+    <div className="critcal_level1">
     </div>
+    <div className="critcal_level2">
+    </div>
+    <div className="critcal_level3">
+    </div>
+    <div className="critcal_level4">
+      
+      </div>
+      </div>
+    
+      </div>
+    
   );
 }
