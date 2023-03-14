e18037d775c1b37b95cd897f494d0bd4a284f07f
sa3dalla <ugs.160345@eng.suez.edu.eg>
2023-03-14 15:59:17 +0200

    employee page design

--- graduation-project/src/pages/application/attendance/Attendace.jsx
+++ graduation-project/src/pages/application/attendance/Attendace.jsx
@@ -6,7 +6,17 @@ import "../style/attendance.css";
 export default function Attendance() {
   return (
     <div className="attendance">
-      <h3>Attendance</h3>
+      <div className="title">
+        <h3>Attendance</h3>
+        <div className="ends">
+          <div className="day">Feb 9</div>
+          <select name="date" id="date">
+            <option value="1">Day</option>
+            <option value="2">Month</option>
+            <option value="3">Year</option>
+          </select>
+        </div>
+      </div>
       <div className="donate">
         <ChartDonate num="20" name="mohamed" age="23" />
         <ChartDonate num="30" />
