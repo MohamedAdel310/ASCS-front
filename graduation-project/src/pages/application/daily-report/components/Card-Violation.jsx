bd1a3f79782057efd16ea5b500b3516d42d49872
MohamedAdel310 <99290531+MohamedAdel310@users.noreply.github.com>
2023-03-14 17:16:05 +0200

    change img location

--- graduation-project/src/pages/application/daily-report/components/Card-Violation.jsx
+++ graduation-project/src/pages/application/daily-report/components/Card-Violation.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-import branchLogo from "F:/ASCS-front/graduation-project/src/assets/image/branch.jpg";
+import branchLogo from "../../../../../src/assets/image/branch.jpg";
 
 export default function CardViolation(props) {
   return (
     <div className="card-violation">
       <div className="card-violation--header">
-      
-      <img id="image" src={branchLogo} alt="branch logo" />
-      
-      <h4 id="text">{props.text}</h4>
-      <div id="number">{props.num}</div>
-      </div>
-      </div>
+        <img id="image" src={branchLogo} alt="branch logo" />
 
+        <h4 id="text">{props.text}</h4>
+        <div id="number">{props.num}</div>
+      </div>
+    </div>
   );
 }
