56c34d591ae89c21b4f4844227401cf7390d2431
sa3dalla <ugs.160345@eng.suez.edu.eg>
2023-03-14 17:29:34 +0200

    title bar

--- graduation-project/src/pages/application/attendance/Attendace.jsx
+++ graduation-project/src/pages/application/attendance/Attendace.jsx
@@ -2,19 +2,31 @@ import React from "react";
 import ChartDonate from "./components/Chart-donate";
 import TableDay from "./components/Table-day";
 import "../style/attendance.css";
-
+import "../../../components/search"
 import employeeData from "./__delete__employeedata.json";
+import Search from "../../../components/search";
 
 export default function Attendance() {
   return (
     <div className="attendance">
-      <h3>Attendance</h3>
+      <div className="title">
+        <h3>Attendance</h3>
+        <div className="ends">
+          <div className="day">Feb 9</div>
+          <select name="date" id="date">
+            <option value="1">Day</option>
+            <option value="2">Month</option>
+            <option value="3">Year</option>
+          </select>
+        </div>
+      </div>
       <div className="donate">
         <ChartDonate num="30" text="Absense" />
         <ChartDonate num="20" text="Late" />
         <ChartDonate num="18" text="Undertime" />
         <ChartDonate num="15" text="Overtime" />
       </div>
+      <Search/>
       <table>
         <thead>
           <tr>
