7f6303afc643d009de458a146a8bbf8dfbf5d3b4
sa3dalla <ugs.160345@eng.suez.edu.eg>
2023-03-15 15:40:50 +0200

    logo style

--- graduation-project/src/pages/application/attendance/components/Filter.jsx
+++ graduation-project/src/pages/application/attendance/components/Filter.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "../../style/filter.css"
 export default function Filter (){
     return(
-        <div className="filter">
+        <button className="filter">
             <h2>Filters</h2>
             <div className="icon">
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
-        </div>
+        </button>
     )
 }
