8efc374396ae2f4e7390b2772973e20face4589f
sa3dalla <ugs.160345@eng.suez.edu.eg>
2023-03-15 15:52:00 +0200

    empolyee filter

--- graduation-project/src/pages/application/employees/Employees.jsx
+++ graduation-project/src/pages/application/employees/Employees.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../style/employees.css";
 import "../../../components/search"
 import Search from "../../../components/search";
+import Filter from "../attendance/components/Filter";
 import TableDay from "../attendance/components/Table-day";
 export default function Employees() {
   return (
@@ -10,6 +11,7 @@ export default function Employees() {
       <div className="add-empolyee">
         <button className="add">Add Employee</button>
         <Search/>
+        <Filter/>
       </div>
       <TableDay/>
     </div>
