150de6d05b2dec7a7708dbfac1c2650eac4a992e
sa3dalla <ugs.160345@eng.suez.edu.eg>
2023-03-15 15:49:40 +0200

    solve error

--- graduation-project/src/main.jsx
+++ graduation-project/src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./pages/application/style/all.min.css";
-import "./pages/application/style/normalize.css";
+// import "./pages/application/style/normalize.css";
 
 import App from "./App";
 
