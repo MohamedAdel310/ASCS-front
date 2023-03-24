c75928470d276554315ed5dc8c3c09d07b53369a
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2023-03-24 06:51:15 +0200

    add random data to employees table
    add random employees data from mockaroo.com
    fix sa3dalla table

--- src/pages/application/employees/Employees.jsx
+++ src/pages/application/employees/Employees.jsx
@@ -1,19 +1,41 @@
 import React from "react";
 import "../style/employees.css";
-import "../../../components/search"
+import "../../../components/search";
+import EmployeeTable from "./components/Empolyees-table";
+import employeesData from "../../../../Data/randomEmployeesData.json";
+
 import Search from "../../../components/search";
 import Filter from "../attendance/components/Filter";
-import EmployeeTable from "./components/Empolyees-table";
+
 export default function Employees() {
   return (
     <div className="employee">
       <h2>Employees</h2>
       <div className="add-empolyee">
         <button className="add">Add Employee</button>
-        <Search/>
-        <Filter/>
+        <Search />
+        <Filter />
       </div>
-      <EmployeeTable/>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Job Title</th>
+            <th>Deparment</th>
+            <th>Job Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employeesData.map((emp) => (
+            <EmployeeTable
+              name={emp.name}
+              jobTitle={emp.job_title}
+              department={emp.department}
+              jobStatus={emp.status ? "Active" : "pending"}
+            />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
