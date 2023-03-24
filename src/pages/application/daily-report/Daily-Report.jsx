a15d4c12b45bbfdc733a27965dcef34645708e44
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2023-03-24 07:37:49 +0200

    add icons to daily-report page
    export icons form XD
    change icon style
    change text in Detected Events section just to look better

--- src/pages/application/daily-report/Daily-Report.jsx
+++ src/pages/application/daily-report/Daily-Report.jsx
@@ -1,54 +1,71 @@
 import React from "react";
 import CardViolation from "./components/Card-Violation";
 import StatusViolation from "./components/Status-Violation";
-import MainButton from "../../../components/button-main";
 import "../style/daily-report.css";
+
+import iconVehicles from "../../../../src/assets/icons/vehicles.svg";
+import iconRestricted from "../../../../src/assets/icons/gloves.svg";
+import iconUnauthorized from "../../../../src/assets/icons/unuothorize.svg";
+import iconSmoking from "../../../../src/assets/icons/smoke.svg";
+import iconSafety from "../../../../src/assets/icons/helmet.svg";
 
 export default function DailyReport() {
   return (
     <div className="daily-report">
-      <div className="card-violation-container">
-        <CardViolation num="8" text="Detected vehicles" />
-        <CardViolation num="3" text="unauthorized persons" />
-        <CardViolation num="5" text="smoking" />
-        <CardViolation num="6" text="safety violation" />
-        <CardViolation num="10" text="restricted area" />
-      </div>
+      <CardViolationComp />
 
       <div className="daily-report--header">
         <h1>Detected events</h1>
       </div>
 
-      <div className="status-box">
-        <StatusViolation
-          level="1"
-          text="Detected vehicles"
-          event="Detected Vehicles"
-          eventMessage="Detected vehicle entered the factory at 19:50 -The plate number
-          is 442 -Reported camera: A70"
-        />
-        <StatusViolation
-          level="3"
-          text="Detected vehicles"
-          event="Detected Vehicles"
-          eventMessage="Detected vehicle entered the factory at 19:50 -The plate number
-          is 442 -Reported camera: A70"
-        />
-        <StatusViolation
-          level="2"
-          text="Detected vehicles"
-          event="Detected Vehicles"
-          eventMessage="Detected vehicle entered the factory at 19:50 -The plate number
-          is 442 -Reported camera: A70"
-        />
-        <StatusViolation
-          level="4"
-          text="Detected vehicles"
-          event="Detected Vehicles"
-          eventMessage="Detected vehicle entered the factory at 19:50 -The plate number
-          is 442 -Reported camera: A70"
-        />
-      </div>
+      <StatusViolationComp />
     </div>
   );
 }
+
+const CardViolationComp = () => {
+  return (
+    <div className="card-violation-container">
+      <CardViolation num="8" text="Detected vehicles" icon={iconVehicles} />
+      <CardViolation
+        num="3"
+        text="unauthorized persons"
+        icon={iconUnauthorized}
+      />
+      <CardViolation num="5" text="smoking" icon={iconSmoking} />
+      <CardViolation num="6" text="safety violation" icon={iconSafety} />
+      <CardViolation num="10" text="restricted area" icon={iconRestricted} />
+    </div>
+  );
+};
+
+const StatusViolationComp = () => {
+  return (
+    <div className="status-box">
+      <StatusViolation
+        level="1"
+        event="Detected Vehicles"
+        eventMessage="Detected vehicle entered the factory at 19:50 -The plate number
+      is 442 -Reported camera: A70"
+      />
+      <StatusViolation
+        level="3"
+        event="Safety Violation"
+        eventMessage="Detected vehicle entered the factory at 19:50 -The plate number
+      is 442 -Reported camera: A70"
+      />
+      <StatusViolation
+        level="2"
+        event="Restricted Area"
+        eventMessage="Detected vehicle entered the factory at 19:50 -The plate number
+      is 442 -Reported camera: A70"
+      />
+      <StatusViolation
+        level="4"
+        event="Unauthorized Person"
+        eventMessage="Detected vehicle entered the factory at 19:50 -The plate number
+      is 442 -Reported camera: A70"
+      />
+    </div>
+  );
+};
--- src/pages/application/daily-report/components/Card-Violation.jsx
+++ src/pages/application/daily-report/components/Card-Violation.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import branchLogo from "../../../../../src/assets/image/branch.jpg";
 
 export default function CardViolation(props) {
   return (
     <div className="card-violation">
-      <img className="card-violation--img" src={branchLogo} alt="branch logo" />
+      <img className="card-violation--img" src={props.icon} alt="branch logo" />
       <div className="mistake">
         <h3 className="card-violation--text">{props.text}</h3>
         <div className="card-violation--number">{props.num}</div>
