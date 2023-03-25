f5a5471a75ea40ecdbca412bd2d1b8640e63d64c
youssefnasserw <yoyozico2016@gmail.com>
2023-03-25 16:18:36 +0200

    last_dash

--- src/pages/application/dashboard/components/LastEventDetected.jsx
+++ src/pages/application/dashboard/components/LastEventDetected.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function LastEventDetected() {
   return (
     <div className="dashboard--last-event-detected">
-      <div>LastEventDetected</div>
+      <div className="last_event_card">Last Event Detected</div>
     </div>
   );
 }
