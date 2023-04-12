fdde19fe5ec3771bb117085a868f514f68828961
sa3dalla <ugs.160345@eng.suez.edu.eg>
2023-04-12 23:12:13 +0200

    Update LastEventDetected.jsx

--- src/pages/application/dashboard/components/LastEventDetected.jsx
+++ src/pages/application/dashboard/components/LastEventDetected.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {bell} from "@fortawesome/free-solid-svg-icons";
 
 export default function LastEventDetected() {
   return (
     <div className="dashboard--last-event-detected">
       <div className="last_event_card">Last Event Detected</div>
+      <div className="personal-detect">
+        <FontAwesomeIcon icon={bell}></FontAwesomeIcon>
+      </div>
+      <div className="vehicle-leaves">
+
+      </div>
+      <div className="vehicle-enters">
+
+      </div>
     </div>
   );
 }
