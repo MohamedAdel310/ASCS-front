20abca10260f4c2f8d5ec352c10cf45abb55814b
sa3dalla <ugs.160345@eng.suez.edu.eg>
2023-04-13 00:53:16 +0200

    cards
    all card style

--- src/pages/application/dashboard/components/Card.jsx
+++ src/pages/application/dashboard/components/Card.jsx
@@ -3,7 +3,10 @@ import React from "react";
 export default function Card(props) {
   return (
     <div className="dashboard--card">
-      <div className="detected_vehicale_card">{props.header}{props.detectedNum}</div>
+      <div className="detected_vehicle_card">{props.header} </div>
+      <div className="detected_vehicle_card_number">{props.detectedNum}</div>
+      <h6>In the last 7 days</h6>
+
      
      
     </div>
