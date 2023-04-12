ceef3c30e1a66517de717743fb87086056c24fd8
sa3dalla <ugs.160345@eng.suez.edu.eg>
2023-04-13 00:32:41 +0200

    last event detected
    all style for last event and jsx

--- src/pages/application/dashboard/components/LastEventDetected.jsx
+++ src/pages/application/dashboard/components/LastEventDetected.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {bell} from "@fortawesome/free-solid-svg-icons";
+import {faBell} from "@fortawesome/free-solid-svg-icons";
 
 export default function LastEventDetected() {
   return (
     <div className="dashboard--last-event-detected">
-      <div className="last_event_card">Last Event Detected</div>
+      <h3>Last Event Detected</h3>
       <div className="personal-detect">
-        <FontAwesomeIcon icon={bell}></FontAwesomeIcon>
+        <FontAwesomeIcon className="bell" icon={faBell}></FontAwesomeIcon>
+        <h4>Unauthorized Personal was detected at ar..</h4>
+        <p>17:40</p>
       </div>
-      <div className="vehicle-leaves">
-
+      <div className="personal-detect">
+        <FontAwesomeIcon className="bell" icon={faBell}></FontAwesomeIcon>
+        <h4>A Vehicle leaves the factory</h4>
+        <p>16:22</p>
       </div>
-      <div className="vehicle-enters">
-
+      <div className="personal-detect">
+        <FontAwesomeIcon className="bell" icon={faBell}></FontAwesomeIcon>
+        <h4>A vehicle enters the factory</h4>
+        <p>16:03</p>
       </div>
+      <button>View More...</button>
     </div>
   );
 }
