6829321db0bc5078c20f51771374d3ca14c65cd0
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2023-04-12 15:26:45 +0200

    get employees form api
    add employees from database to employees page
    
    >> change token to take it from local storage

--- src/pages/application/employees/Employees.jsx
+++ src/pages/application/employees/Employees.jsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../style/employees.css";
 import "../../../components/search";
 import EmployeeTable from "./components/Empolyees-table";
-import employeesData from "../../../../Data/randomEmployeesData.json";
+// import employeesData from "../../../../Data/randomEmployeesData.json";
 
 import Search from "../../../components/search";
 import Filter from "../../../components/Filter";
 
+const apiURL = "https://myaz.cyclic.app/api/";
+
 export default function Employees() {
+  const [data, setData] = useState();
+  const [employeesData, setEmployeesData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log("fetch done");
+      const token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZjc3ZjUzYzZmYzhmN2IxYzUzYzc3MSIsImlhdCI6MTY3OTc1NDU0OCwiZXhwIjoxNjg3NTMwNTQ4fQ.pblQyXpHTH6QdITy1fASGmOtmjCDtVptFnRBNsDs9EM";
+      const headers = {
+        "Authorization": `Bearer ${token}`,
+      };
+      const response = await fetch(apiURL + "employees", {
+        headers,
+      });
+      const json = await response.json();
+      setData(json.data);
+
+      localStorage.setItem("employeesData", JSON.stringify(json.data));
+      setEmployeesData(json.data?.employees);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="employee">
       <h2>Employees</h2>
@@ -26,10 +51,11 @@ export default function Employees() {
           </tr>
         </thead>
         <tbody>
-          {employeesData.map((emp) => (
+          {employeesData?.map((emp) => (
             <EmployeeTable
+              key={emp.employee_id}
               name={emp.name}
-              jobTitle={emp.job_title}
+              jobTitle={emp.job}
               department={emp.department}
               jobStatus={emp.status ? "Active" : "pending"}
             />
