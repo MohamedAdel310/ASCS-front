90662f745009d24541feff337f2fd1fd8be5acc8
sa3dalla <ugs.160345@eng.suez.edu.eg>
2023-04-16 00:36:42 +0200

    add employee button

--- src/pages/application/employees/components/PopupAddEmployee.jsx
+++ src/pages/application/employees/components/PopupAddEmployee.jsx
@@ -1,14 +1,27 @@
+/** @format */
+
 import React from "react";
 import MainButton from "../../../../components/button-main";
 
 export default function PopupAddEmployee(props) {
   return (
     <div className="popup--add-employee">
-      <h1>hello from add employee popup</h1>
-      <MainButton text="add" onClick={() => props.handelCallback(false)} />
-      <MainButton text="close" onClick={() => props.handelCallback(false)} />
-      {/* <MainButton text="add" onClick={() => props.handelCallback(false)} />
-      <MainButton text="close" onClick={() => props.handelCallback(false)} /> */}
+      <h1>Add Employee</h1>
+      <input type="text" placeholder="Name" />
+      <input type="text" placeholder="Department" />
+      <input type="text" placeholder="Job" />
+      <input type="text" placeholder="Email" />
+      <input type="text" placeholder="Phone" />
+      <label htmlFor="">Gender</label>
+      <select name="Gender" id="" >
+        <option value="">Male</option>
+        <option value="">Famale</option>
+      </select>
+      <input type="text" placeholder="Salary" />
+      <div className="close-and-add">
+        <MainButton text="add" onClick={() => props.handelCallback(false)} />
+        <MainButton text="close" onClick={() => props.handelCallback(false)} />
+      </div>
     </div>
   );
 }
