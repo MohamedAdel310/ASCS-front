d92cd3908b5cd712ce5cd9e6728024381687f624
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2023-04-17 13:47:21 +0200

    fix login enimation from  sa3dalla code

--- src/pages/landing/contact-us.jsx
+++ src/pages/landing/contact-us.jsx
@@ -1,113 +1,109 @@
 import React from "react";
-import "./contact.css"
+import "./contact.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
+export default function Contact() {
+  return (
+    <div class="contact" id="contact">
+      <div class="container">
+        <span class="big-circle"></span>
+        <img src="img/shape.png" class="square" alt="" />
+        <div class="form">
+          <div class="contact-info">
+            <h3 class="title">Let's get in touch</h3>
+            <p class="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
+              dolorum adipisci recusandae praesentium dicta!
+            </p>
 
-
-
-export default function Contact(){
-    return(
-        <div class="contact" id="contact">
-            <div class="container">
-                <span class="big-circle"></span>
-                <img src="img/shape.png" class="square" alt="" />
-                <div class="form">
-                <div class="contact-info">
-                    <h3 class="title">Let's get in touch</h3>
-                    <p class="text">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe
-                        dolorum adipisci recusandae praesentium dicta!
-                    </p>
-
-                    <div class="info">
-                    <div class="information">
-                        <img src="img/location.png" class="icon" alt="" />
-                        <p>92 Cherry Drive Uniondale, NY 11553</p>
-                    </div>
-                    <div class="information">
-                        <img src="img/email.png" class="icon" alt="" />
-                        <p>lorem@ipsum.com</p>
-                    </div>
-                    <div class="information">
-                        <img src="img/phone.png" class="icon" alt="" />
-                        <p>123-456-789</p>
-                    </div>
-                    </div>
-
-                    <div class="social-media">
-                    <p>Connect with us :</p>
-                    <div class="social-icons">
-                        <a href="#">
-                            <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
-                        </a>
-                        <a href="#">
-                            <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
-                        </a>
-                        <a href="#">
-                            <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
-                        </a>
-                        <a href="#">
-                            <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
-                        </a>
-                    </div>
-                    </div>
-                </div>
-
-                <div class="contact-form">
-                    <span class="circle one"></span>
-                    <span class="circle two"></span>
-
-                    <form action="index.html" autocomplete="off">
-                    <h3 class="title">Contact us</h3>
-                    <div class="input-container">
-                        <input type="text" name="name" class="input" />
-                        <label for="">Username</label>
-                        <span>Username</span>
-                    </div>
-                    <div class="input-container">
-                        <input type="email" name="email" class="input" />
-                        <label for="">Email</label>
-                        <span>Email</span>
-                    </div>
-                    <div class="input-container">
-                        <input type="tel" name="phone" class="input" />
-                        <label for="">Phone</label>
-                        <span>Phone</span>
-                    </div>
-                    <div class="input-container textarea">
-                        <textarea name="message" class="input"></textarea>
-                        <label for="">Message</label>
-                        <span>Message</span>
-                    </div>
-                    <input type="submit" value="Send" class="btn" />
-                    </form>
-                </div>
-                </div>
+            <div class="info">
+              <div class="information">
+                <img src="img/location.png" class="icon" alt="" />
+                <p>92 Cherry Drive Uniondale, NY 11553</p>
+              </div>
+              <div class="information">
+                <img src="img/email.png" class="icon" alt="" />
+                <p>lorem@ipsum.com</p>
+              </div>
+              <div class="information">
+                <img src="img/phone.png" class="icon" alt="" />
+                <p>123-456-789</p>
+              </div>
             </div>
+
+            <div class="social-media">
+              <p>Connect with us :</p>
+              <div class="social-icons">
+                <a href="#">
+                  <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
+                </a>
+                <a href="#">
+                  <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+                </a>
+                <a href="#">
+                  <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+                </a>
+                <a href="#">
+                  <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="contact-form">
+            <span class="circle one"></span>
+            <span class="circle two"></span>
+
+            <form action="index.html" autocomplete="off">
+              <h3 class="title">Contact us</h3>
+              <div class="input-container">
+                <input type="text" name="name" class="input" />
+                <label for="">Username</label>
+                <span>Username</span>
+              </div>
+              <div class="input-container">
+                <input type="email" name="email" class="input" />
+                <label for="">Email</label>
+                <span>Email</span>
+              </div>
+              <div class="input-container">
+                <input type="tel" name="phone" class="input" />
+                <label for="">Phone</label>
+                <span>Phone</span>
+              </div>
+              <div class="input-container textarea">
+                <textarea name="message" class="input"></textarea>
+                <label for="">Message</label>
+                <span>Message</span>
+              </div>
+              <input type="submit" value="Send" class="btn" />
+            </form>
+          </div>
         </div>
-        
-    )
+      </div>
+    </div>
+  );
 }
 
 const inputs = document.querySelectorAll(".input");
 
 function focusFunc() {
-    let parent = this.parentNode;
-    parent.classList.add("focus");
+  let parent = this.parentNode;
+  parent.classList.add("focus");
 }
 
 function blurFunc() {
-    let parent = this.parentNode;
-    if (this.value == "") {
+  let parent = this.parentNode;
+  if (this.value == "") {
     parent.classList.remove("focus");
-    }
+  }
 }
 
 inputs.forEach((input) => {
-    input.addEventListener("focus", focusFunc);
-    input.addEventListener("blur", blurFunc);
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
 });
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,80 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
 
-
-
-// const signUpButton = document.getElementById("signUp");
-// const signInButton = document.getElementById("signIn");
-// const container = document.getElementById("login-page");
-
-// signUpButton.addEventListener("click", () => {
-//   container.classList.add("right-panel-action");
-// });
-// signInButton.addEventListener("click", () => {
-//   container.classList.remove("right-panel-action");
-// });
 export default function Login() {
+  const [containerClass, setContainerClass] = useState("login-page");
+
+  const handleAddClass = () => {
+    setContainerClass("login-page right-panel-action");
+  };
+
+  const handleRemoveClass = () => {
+    setContainerClass("login-page");
+  };
+
   return (
     <div className="login-home">
-      <div class="login-page" id="login-page">
-      <div class="form-container sign-up">
-        <form action="#">
-          <h1>Create an account</h1>
-          <div class="social-container">
-            <a href="#">
-              <i class="fab fa-facebook"></i>
-            </a>
-            <a href="#">
-              <i class="fab fa-google"></i>
-            </a>
-            <a href="#">
-              <i class="fab fa-github"></i>
-            </a>
-          </div>
-          <span>use your email for register</span>
-          <input type="text" placeholder="Name" required />
-          <input type="email" placeholder="ُEmail" required />
-          <input type="password" placeholder="Password" required />
-          <br />
-          <button>Sign Up</button>
-        </form>
-      </div>
-      <div class="form-container sign-in">
-        <form action="#">
-          <h1>Sign in</h1>
-          <div class="social-container">
-            <a href="#">
-              <i class="fab fa-facebook"></i>
-            </a>
-            <a href="#">
-              <i class="fab fa-google"></i>
-            </a>
-            <a href="#">
-              <i class="fab fa-github"></i>
-            </a>
-          </div>
-          <span>login to your account</span>
-          <input type="email" placeholder="ُEmail" required />
-          <input type="password" placeholder="Password" required />
-          <a href="#">Forget Your Password?</a>
-          <button><a href="./brance.html">Sign In</a></button>
-        </form>
-      </div>
-      <div class="overlay-container">
-        <div class="overlay">
-          <div class="overlay-panel overlay-left">
-            <h1>Have already account?</h1>
-            <p>To Keep connect with us please login with your account</p>
-            <button id="signIn" class="ghost">Sign In</button>
-          </div>
-          <div class="overlay-panel overlay-right">
-            <h1>Create Account</h1>
-            <p>Enter your personal detail and start journey with us</p>
-            <button id="signUp" class="ghost" >Sign Up</button>
+      <div class={containerClass} id="login-page">
+        <div class="form-container sign-up">
+          <form action="#">
+            <h1>Create an account</h1>
+            <div class="social-container">
+              <a href="#">
+                <i class="fab fa-facebook"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-google"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-github"></i>
+              </a>
+            </div>
+            <span>use your email for register</span>
+            <input type="text" placeholder="Name" required />
+            <input type="email" placeholder="ُEmail" required />
+            <input type="password" placeholder="Password" required />
+            <br />
+            <button>Sign Up</button>
+          </form>
+        </div>
+        <div class="form-container sign-in">
+          <form action="#">
+            <h1>Sign in</h1>
+            <div class="social-container">
+              <a href="#">
+                <i class="fab fa-facebook"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-google"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-github"></i>
+              </a>
+            </div>
+            <span>login to your account</span>
+            <input type="email" placeholder="ُEmail" required />
+            <input type="password" placeholder="Password" required />
+            <a href="#">Forget Your Password?</a>
+            <button>
+              <a href="./brance.html">Sign In</a>
+            </button>
+          </form>
+        </div>
+        <div class="login-overlay-container">
+          <div class="login-overlay">
+            <div class="login-overlay-panel login-overlay-left">
+              <h1>Have already account?</h1>
+              <p>To Keep connect with us please login with your account</p>
+              <button id="signIn" class="ghost" onClick={handleRemoveClass}>
+                Sign In
+              </button>
+            </div>
+            <div class="login-overlay-panel login-overlay-right">
+              <h1>Create Account</h1>
+              <p>Enter your personal detail and start journey with us</p>
+              <button id="signUp" class="ghost" onClick={handleAddClass}>
+                Sign Up
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
