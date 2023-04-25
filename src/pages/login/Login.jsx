17489a5c64562dbf74821119becc505aff547de2
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2023-04-25 19:06:05 +0200

    check from api about user email and password
    if ok save token in local storage
    redirect to (/application)
    
    if it not don't do any thing

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,34 +1,68 @@
 import React, { useState } from "react";
 import "./login.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faInstagram, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Login() {
-  const [containerClass, setContainerClass] = useState("login-page");
+  const [containerclass, setContainerclass] = useState("login-page");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState("");
 
-  const handleAddClass = () => {
-    setContainerClass("login-page right-panel-action");
+  const handleAddclass = () => {
+    setContainerclass("login-page right-panel-action");
   };
 
-  const handleRemoveClass = () => {
-    setContainerClass("login-page");
+  const handleRemoveclass = () => {
+    setContainerclass("login-page");
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch("https://myaz.cyclic.app/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log("Login successful!");
+          return response.json();
+          // redirect to the user's dashboard or home page
+        } else {
+          console.error("Login failed.");
+        }
+      })
+      .then((res) => {
+        setToken(res.token);
+        localStorage.setItem("token", res.token); // save token to local storage
+        console.log(token);
+      })
+      .catch((error) => console.error(error));
   };
 
   return (
     <div className="login-home">
-      <div class={containerClass} id="login-page">
-        <div class="form-container sign-up">
+      <div className={containerclass} id="login-page">
+        <div className="form-container sign-up">
           <form action="#">
             <h1>Create an account</h1>
-            <div class="social-container">
+            <div className="social-container">
               <a href="#">
-              <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
               </a>
               <a href="#">
-              <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
               </a>
               <a href="#">
-              <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
               </a>
             </div>
             <span>use your email for register</span>
@@ -39,48 +73,59 @@ export default function Login() {
             <button>Sign Up</button>
           </form>
         </div>
-        <div class="form-container sign-in">
-          <form action="#">
+        <div className="form-container sign-in">
+          <form onSubmit={handleSubmit}>
             <h1>Sign in</h1>
-            <div class="social-container">
+            <div className="social-container">
               <a href="#">
                 <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
               </a>
               <a href="#">
-              <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
               </a>
               <a href="#">
-              <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+                <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
               </a>
             </div>
             <span>login to your account</span>
-            <input type="email" placeholder="ُEmail" required />
-            <input type="password" placeholder="Password" required />
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
             <a href="#">Forget Your Password?</a>
-            <button>
-              <a href="./brance.html">Sign In</a>
-            </button>
+            <button type="submit">Sign In</button>
           </form>
         </div>
-        <div class="login-overlay-container">
-          <div class="login-overlay">
-            <div class="login-overlay-panel login-overlay-left">
+        <div className="login-overlay-container">
+          <div className="login-overlay">
+            <div className="login-overlay-panel login-overlay-left">
               <h1>Have already account?</h1>
               <p>To Keep connect with us please login with your account</p>
-              <button id="signIn" class="ghost" onClick={handleRemoveClass}>
+              <button id="signIn" className="ghost" onClick={handleRemoveclass}>
                 Sign In
               </button>
             </div>
-            <div class="login-overlay-panel login-overlay-right">
+            <div className="login-overlay-panel login-overlay-right">
               <h1>Create Account</h1>
               <p>Enter your personal detail and start journey with us</p>
-              <button id="signUp" class="ghost" onClick={handleAddClass}>
+              <button id="signUp" className="ghost" onClick={handleAddclass}>
                 Sign Up
               </button>
             </div>
           </div>
         </div>
       </div>
+      {token && (window.location.href = "/application")}
     </div>
   );
 }
