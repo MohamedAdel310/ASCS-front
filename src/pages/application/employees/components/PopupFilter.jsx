f7735c332267f7fb124bfd0a2ba02475c612676f
sa3dalla <104719925+sa3dalla@users.noreply.github.com>
2023-05-10 21:16:02 +0300

    Filter-popup style

--- src/pages/application/employees/components/PopupFilter.jsx
+++ src/pages/application/employees/components/PopupFilter.jsx
@@ -7,12 +7,35 @@ export default function PopupFilter(props) {
   };
 
   return (
-    <div
-      className={`popup--filter popup ${props.value || "display-popup-false"}`}
-    >
-      <h1>hello from add filter popup</h1>
-      <MainButton text="add" onClick={handelPopupFilter} />
-      <MainButton text="close" onClick={handelPopupFilter} />
+    <div className={`popup--filter popup ${props.value || "display-popup-false"}`}>
+      <h1>Filter</h1>
+    {/************************ jop Title *************************** */}
+      <div className="department">
+          <label htmlFor="">jop Title</label>
+          <select name="title" id="">
+            <option value="">Worker</option>
+            <option value="">Engineering</option>
+            <option value="">Acountant</option>
+            <option value="">Doctor</option>
+          </select>
+      </div>
+    {/* ****************************department **************************/}
+      <div className="department">
+          <label htmlFor="">Department Title</label>
+          <select name="Department" id="">
+            <option value="">Office</option>
+            <option value="">Area 1</option>
+            <option value="">Area 2</option>
+            <option value="">Area 3</option>
+            <option value="">Area 4</option>
+          </select>
+      </div>
+    {/**************************** Buttons ****************************/}
+
+      <div className="close-and-add">
+        <MainButton text="add" onClick={handelPopupFilter} />
+        <MainButton text="close" onClick={handelPopupFilter} />
+      </div>
     </div>
   );
 }
