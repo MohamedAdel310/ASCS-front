73b5e2571252cf94e7658d64cfb02f8f07df3076
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2023-05-26 00:36:17 +0300

    edit employee details DOOLA IDEA

--- src/pages/application/employees/components/Empolyee-profile.jsx
+++ src/pages/application/employees/components/Empolyee-profile.jsx
@@ -16,7 +16,35 @@ import { useParams } from "react-router-dom";
 export default function EmpProfile() {
   const { id } = useParams();
   const [employee, setEmployee] = useState(null);
-  const [openEditDetailsPopup, setOpenEditDetailsPopup] = useState(false);
+  const [employeeAnother, setEmployeeAnother] = useState(null);
+  const [inputField, setInputField] = useState({});
+  // const [openEditDetailsPopup, setOpenEditDetailsPopup] = useState(false);
+  const [canEdit, setCanEdit] = useState(false);
+  const [btnHidden, setBtnHidden] = useState(true);
+
+  const handleChangeInput = (e, type) => {
+    setInputField({ ...inputField, [type]: e.target.value });
+    console.log(inputField);
+  };
+
+  const handleClickEdit = () => {
+    setCanEdit(true);
+    setBtnHidden(false);
+  };
+
+  const handleClickSave = () => {
+    setCanEdit(false);
+    setBtnHidden(true);
+    setEmployeeAnother(employee);
+    setInputField(null);
+  };
+
+  const handleClickCancel = () => {
+    setCanEdit(false);
+    setBtnHidden(true);
+    setEmployeeAnother(employee);
+    setInputField(null);
+  };
 
   useEffect(() => {
     const fetchEmployee = async () => {
@@ -52,56 +80,123 @@ export default function EmpProfile() {
             <img src={imployeeImg} alt="" />
           </div>
           <div className="information">
-            <h3>{employee?.name}</h3>
+            {/* <h3>{employee?.name}</h3> */}
             <div className="label-container">
               <input
                 type="text"
-                placeholder={employee?.phoneNumber || "there is no phone"}
-                readOnly
+                value={
+                  // if user delete all name condition add name so I do this so if there is no name it will apear no name
+                  inputField?.name === ""
+                    ? ""
+                    : inputField?.name || employee?.name || "there is no Name"
+                  // if (name) is false and no empty, type employee name
+                }
+                readOnly={!canEdit}
+                onChange={(e) => handleChangeInput(e, "name")}
+              />
+              <label htmlFor="">Name</label>
+              <div className="label-container"></div>
+              <input
+                type="text"
+                value={
+                  inputField?.phoneNumber === ""
+                    ? ""
+                    : inputField?.phoneNumber ||
+                      employee?.phoneNumber ||
+                      "there is no phone"
+                }
+                readOnly={!canEdit}
+                onChange={(e) => handleChangeInput(e, "phoneNumber")}
               />
               <label htmlFor="">Phone Number</label>
             </div>
             <div className="label-container">
-              <input type="text" placeholder={employee?.job} readOnly />
+              <input
+                type="text"
+                value={
+                  inputField?.job === "" ? "" : inputField?.job || employee?.job
+                }
+                readOnly={!canEdit}
+                onChange={(e) => handleChangeInput(e, "job")}
+              />
               <label htmlFor="">Job Title</label>
             </div>
             <div className="label-container">
-              <input type="text" placeholder={employee?.department} readOnly />
+              <input
+                type="text"
+                value={
+                  inputField?.department === ""
+                    ? ""
+                    : inputField?.department || employee?.department
+                }
+                readOnly={!canEdit}
+                onChange={(e) => handleChangeInput(e, "department")}
+              />
               <label htmlFor="">Department</label>
             </div>
-          
-           
           </div>
           <div className="shift">
             <div className="label-container">
               <input
                 type="email"
-                placeholder={employee?.email || "there is no email"}
-                readOnly
+                value={
+                  inputField?.email === ""
+                    ? ""
+                    : inputField?.email ||
+                      employee?.email ||
+                      "there is no email"
+                }
+                readOnly={!canEdit}
+                onChange={(e) => handleChangeInput(e, "email")}
               />
               <label htmlFor="">Email</label>
             </div>
             <div className="label-container">
               <input
                 type="text"
-                placeholder={employee?.shiftStartAt || "00:00 "}
-                readOnly
+                value={
+                  inputField?.shiftStartAt === ""
+                    ? ""
+                    : inputField?.shiftStartAt ||
+                      employee?.shiftStartAt ||
+                      "00:00 "
+                }
+                readOnly={!canEdit}
+                onChange={(e) => handleChangeInput(e, "shiftStartAt")}
               />
               <label htmlFor="">Shift Start at</label>
             </div>
             <div className="label-container">
               <input
                 type="text"
-                placeholder={employee?.shiftEndAt || "00:00 "}
-                readOnly
+                value={
+                  inputField?.shiftEndAt === ""
+                    ? ""
+                    : inputField?.shiftEndAt || employee?.shiftEndAt || "00:00 "
+                }
+                readOnly={!canEdit}
+                onChange={(e) => handleChangeInput(e, "shiftEndAt")}
               />
               <label htmlFor="">Shift End at</label>
             </div>
+            <div className="label-container">
+              <input
+                type="number"
+                value={
+                  inputField?.salary === ""
+                    ? ""
+                    : inputField?.salary || employee?.salary || "00:00 "
+                }
+                readOnly={!canEdit}
+                onChange={(e) => handleChangeInput(e, "salary")}
+              />
+              <label htmlFor="">Salary</label>
+            </div>
           </div>
           <div className="date">
-            <h5>
+            {/* <h5>
               Salary: <span>{employee?.salary || "0000"}EGP</span>
-            </h5>
+            </h5> */}
             <div className="circle">
               <h5>
                 Status: <span>{employee?.status || "NULL"}</span>
@@ -117,20 +212,36 @@ export default function EmpProfile() {
               <span>{employee?.lastEdit.slice(0, 10) || "0000-00-00"}</span>
             </h5>
           </div>
-          
         </div>
         <div className="btn--edit-details--">
-            <button
-              className="btn--edit-details"
-              onClick={() => setOpenEditDetailsPopup(true)}
-            >
-              Edit Details
-            </button>
-            </div>
-            <PopupEditDetails
-              active={openEditDetailsPopup}
-              setOpenEditDetailsPopup={setOpenEditDetailsPopup}
-            />
+          <button
+            className="btn--edit-details"
+            onClick={handleClickEdit}
+            style={{ fontSize: "14px" }}
+            hidden={!btnHidden}
+          >
+            Edit Details
+          </button>
+          <button
+            className="btn--edit-details"
+            onClick={handleClickSave}
+            hidden={btnHidden}
+          >
+            Save
+          </button>
+          <button
+            className="btn--edit-details"
+            onClick={handleClickCancel}
+            hidden={btnHidden}
+            style={{ background: "red" }}
+          >
+            Cancel
+          </button>
+        </div>
+        {/* <PopupEditDetails
+          active={canEdit}
+          setCanEdit={setCanEdit}
+        /> */}
         <div className="overview">
           <div className="title">
             <h3>Overview</h3>
