a0770e990618754cab8c076a2b44eed9c8814e1f
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2023-06-27 16:11:50 +0300

    change youssef coded for stream page

--- src/pages/application/stream/Stream.jsx
+++ src/pages/application/stream/Stream.jsx
@@ -4,55 +4,63 @@ import "../style/stream.css";
 export default function Stream() {
   return (
     <div className="allcams">
-      <div className="header">
-    <h1>All Cams</h1>
-    <div className="selectscale">
-    <select
-            name="Scale"
-           id="scale"
-          >
-            <option value="1">3*3</option>
-            <option value="2">5*5</option>
-            <option value="3">6*6</option>
+      <div className="stream--header">
+        <h1>All Cams</h1>
+        <div className="select-scale--container">
+          <select name="Scale" className="select-scale">
+            <option value="1">3 * 3</option>
+            <option value="2">5 * 5</option>
+            <option value="3">6 * 6</option>
           </select>
-          </div>
-  <div className="cam_no1">
-    <video controls 
-    height="800"
-    width="1400"
-        >
-    </video>
+        </div>
+        <div className="stream--videos-container">
+          <video
+            controls
+            src=""
+            className="stream--video stream--video-main"
+          ></video>
+          <video
+            controls
+            src=""
+            className="stream--video stream--video-1"
+          ></video>
+          <video
+            controls
+            src=""
+            className="stream--video stream--video-2"
+          ></video>
+          <video
+            controls
+            src=""
+            className="stream--video stream--video-3"
+          ></video>
+          <video
+            controls
+            src=""
+            className="stream--video stream--video-4"
+          ></video>
+          <video
+            controls
+            src=""
+            className="stream--video stream--video-5"
+          ></video>
+          <video
+            controls
+            src=""
+            className="stream--video stream--video-6"
+          ></video>
+          <video
+            controls
+            src=""
+            className="stream--video stream--video-7"
+          ></video>
+          <video
+            controls
+            src=""
+            className="stream--video stream--video-8"
+          ></video>
+        </div>
+      </div>
     </div>
-  </div>
-<div className="cams">
-
-            <video id="Cam-no2" width="340" height="300" title="cam1" controls >
-                
-            </video>
-            <video id="Cam-no3" width="340" height="300" controls>
-                
-            </video>
-            <video id="Cam-no4" width="340" height="300" controls autoPlay>
-                <source src="https://www.youtube.com/watch?v=uT-xxeFkgO8" type="ogg"></source>
-            </video>
-            <video id="Cam-no5" width="340" height="300" controls >
-                
-            </video>
-     
-            <video id="Cam-no6" width="340" height="300" controls >
-                
-            </video>
-            <video id="Cam-no7" width="340" height="300"  controls >
-            </video>
-            <video id="Cam-no8" width="340" height="300"  controls >
-                
-            </video>
-            <video id="Cam-no9" width="340"  height="300" controls >
-                
-            </video>
-        
- </div>
- </div>
   );
-  
-};
+}
