bdd19167ba4f29d0b4f201b7897156d4d731e3b2
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2023-07-01 14:54:58 +0300

    make stream work

--- src/pages/application/stream/Stream.jsx
+++ src/pages/application/stream/Stream.jsx
@@ -1,7 +1,50 @@
 import React from "react";
 import "../style/stream.css";
+import VideoJS from "./components/StreamConfig";
 
 export default function Stream() {
+  const playerRef = React.useRef(null);
+
+  const videoJsOptions = {
+    autoplay: false,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    poster: "imgURL",
+    sources: [
+      {
+        src: "http://20.111.28.107:8088/hls/myaz.m3u8",
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+
+  const videoJsOptionsMain = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: "http://20.111.28.107:8088/hls/myaz.m3u8",
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on("waiting", () => {
+      console.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      console.log("player will dispose");
+    });
+  };
+
   return (
     <div className="allcams">
       <div className="stream--header">
@@ -13,55 +56,37 @@ export default function Stream() {
             <option value="3">6 * 6</option>
           </select>
         </div>
+      </div>
+      <div className="stream--videos-container">
+        <div className="stream--video stream--video-main">
+          <VideoJS options={videoJsOptionsMain} onReady={handlePlayerReady} />
         </div>
-        <div className="stream--videos-container">
-          <video
-            controls
-            src=""
-            className="stream--video stream--video-main"
-          ></video>
-          <video
-            controls
-            src=""
-            className="stream--video stream--video-1"
-          ></video>
-          <video
-            controls
-            src=""
-            className="stream--video stream--video-2"
-          ></video>
-          <video
-            controls
-            src=""
-            className="stream--video stream--video-3"
-          ></video>
-          <video
-            controls
-            src=""
-            className="stream--video stream--video-4"
-          ></video>
-          <video
-            controls
-            src=""
-            className="stream--video stream--video-5"
-          ></video>
-          <video
-            controls
-            src=""
-            className="stream--video stream--video-6"
-          ></video>
-          <video
-            controls
-            src=""
-            className="stream--video stream--video-7"
-          ></video>
-          <video
-            controls
-            src=""
-            className="stream--video stream--video-8"
-          ></video>
+
+        <div className="stream--video stream--video-1">
+          <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
         </div>
-     
+        <div className="stream--video stream--video-2">
+          <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+        </div>
+        <div className="stream--video stream--video-3">
+          <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+        </div>
+        <div className="stream--video stream--video-4">
+          <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+        </div>
+        <div className="stream--video stream--video-5">
+          <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+        </div>
+        <div className="stream--video stream--video-6">
+          <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+        </div>
+        <div className="stream--video stream--video-7">
+          <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+        </div>
+        <div className="stream--video stream--video-8">
+          <VideoJS options={videoJsOptions} onReady={handlePlayerReady} />
+        </div>
+      </div>
     </div>
   );
 }
