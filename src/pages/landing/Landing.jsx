1e4de64fec6ab17ad1231bbb80105fd225ffa149
sa3dalla <104719925+sa3dalla@users.noreply.github.com>
2023-07-03 01:32:04 +0300

    landing style

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./landing.css";
-import Contact from"./contact-us";
+import Contact from "./contact-us";
 import About from "./about-us";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDoubleDown } from "@fortawesome/free-solid-svg-icons";
@@ -8,47 +8,49 @@ import { faAngleDoubleDown } from "@fortawesome/free-solid-svg-icons";
 export default function Landing() {
   return (
     <div>
+      <div className="landing-image">
       <div className="landing">
-        <div className="box1"></div>
-        <div className="box2">
-          <div className="text1">
-            AI surveillance
-            <br />
-            camera system
-          </div>
-        </div>
-        <div className="box3">
-            <div className="logo--image">
-              <img src="/public/myaz.png" alt="" />
+          <div className="box1"></div>
+            <div className="box2">
+              <div className="text1">
+                AI surveillance
+                <br />
+                camera system
+              </div>
             </div>
-          <div className="container">
-            <div className="menu">
-              <ul>
-                <li>
-                  <a href="#">Home</a>
-                </li>
-                <li>
-                  <a href="#">Service</a>
-                </li>
-                <li>
-                  <a href="#about-us">About Us</a>
-                </li>
-                <li>
-                  <a href="#contact">Contact Us</a>
-                </li>
-                <li className="login">
-                  <a href="/login">Login</a>
-                </li>
-              </ul>
+            <div className="box3">
+              <div className="logo--image">
+                <img src="/public/myaz.png" alt="" />
+              </div>
+              <div className="container">
+                <div className="menu">
+                  <ul>
+                    <li>
+                      <a href="#">Home</a>
+                    </li>
+                    <li>
+                      <a href="#">Service</a>
+                    </li>
+                    <li>
+                      <a href="#about-us">About Us</a>
+                    </li>
+                    <li>
+                      <a href="#contact">Contact Us</a>
+                    </li>
+                    <li className="login">
+                      <a href="/login">Login</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <a href="#contact" className="go-down">
-          <FontAwesomeIcon icon={faAngleDoubleDown} ></FontAwesomeIcon>
-        </a>
+            <a href="#contact" className="go-down">
+              <FontAwesomeIcon icon={faAngleDoubleDown} ></FontAwesomeIcon>
+            </a>
       </div>
-      <Contact/>
-      <About/>
+      </div>
+      <Contact />
+      <About />
     </div>
   );
 }
