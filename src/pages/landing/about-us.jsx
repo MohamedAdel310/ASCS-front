02ff4165fd5cdc5e72836edd20bb29f7ea21fef3
sa3dalla <104719925+sa3dalla@users.noreply.github.com>
2023-07-10 02:27:54 +0300

    landing

--- src/pages/landing/about-us.jsx
+++ src/pages/landing/about-us.jsx
@@ -15,14 +15,7 @@ export default function About(){
                     <div class="content">
                         <h3>AI surveillance camera system.</h3>
                         <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
-                        explicabo iusto autem perferendis odit ducimus aut accusamus,
-                        voluptatibus aliquid officia dolores recusandae sequi distinctio
-                        eligendi illum optio non praesentium architecto? Lorem ipsum dolor
-                        sit amet consectetur adipisicing elit. Eveniet maiores hic dicta
-                        quae doloremque suscipit praesentium, eos eligendi doloribus sequi
-                        expedita? Quasi magni accusantium nostrum repellendus illum.
-                        Dolorem, eum numquam?
+                        surveillance cameras have become ubiquitous, capturing a multitude of events throughout the day. Their widespread presence has significantly contributed to ensuring the safety of various public spaces, such as streets, city squares, hospitals, banks, and courts. As a result, the demand for video surveillance systems in these areas has been steadily rising, primarily driven by the objective of enhancing security. Given the paramount importance of safety and security, our focus has centered around three key tasks in this realm. Firstly, we prioritize the detection of personal protective equipment, ensuring that individuals adhere to safety guidelines by wearing appropriate gear. Secondly, we aim to detect any intrusions or unauthorized access, promptly alerting relevant authorities to prevent potential threats. Lastly, we emphasize the detection of unwanted activities, enabling timely intervention to address any prevented behaviors. By addressing these crucial aspects through video surveillance systems, we strive to create a safer environment for everyone in public places, fostering a sense of security and peace of mind.
                         </p>
                         <div class="button">
                         <a href="">Read More</a>
