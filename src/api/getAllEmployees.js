b73f9375c2e93580bd08f0b5a29a4dfbab56cb97
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-01-27 14:19:01 +0200

    ADD: error handle to get employees request

--- src/api/getAllEmployees.js
+++ src/api/getAllEmployees.js
@@ -1,16 +1,28 @@
 const token = localStorage.getItem('token');
 const api = import.meta.env.VITE_API;
 
-export default async function fetchData(setEmployeesData, setIsLoaded) {
-  const headers = {
-    'Authorization': `Bearer ${token}`,
-  };
-  const response = await fetch(api + '/employees', {
-    headers,
-  });
+const headers = {
+  'Authorization': `Bearer ${token}`,
+};
 
-  const res = await response.json();
-  const data = res.data;
-  setEmployeesData(data?.employees);
-  setIsLoaded(true);
+export default async function fetchData(setEmployeesData, setIsLoaded) {
+  try {
+    setIsLoaded(false);
+    const response = await fetch(api + '/employees', {
+      headers,
+    });
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error('');
+    }
+
+    const res = await response.json();
+    const data = res.data;
+    setEmployeesData(data?.employees);
+  } catch {
+    alert('error happened when get employees data');
+  } finally {
+    setIsLoaded(true);
+  }
 }
