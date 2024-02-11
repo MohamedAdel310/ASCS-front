924c85c85ed2a0014b4e2bf0af04b96e2e6dbc6e
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-11 22:10:40 +0200

    CLEAN_STYLE : landing style and delete duplicate code

--- src/pages/application/attendance/components/ChartDonat.jsx
+++ src/pages/application/attendance/components/ChartDonat.jsx
@@ -46,7 +46,6 @@ const DonutCharts = (props) => {
         <svg width="100%" height="100%" viewBox="0 0 200 200">
           <circle style={circleStyles} cx="100" cy="100" r={radius} />
           <circle style={circleStylesFilled} cx="100" cy="100" r={radius} />
-
           <text style={textStyles}>{props.percent}</text>
         </svg>
       </div>
