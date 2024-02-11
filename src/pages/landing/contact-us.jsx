1ecd5a7bfa35cb38d63d892d6d3a67d65079d6a8
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-11 23:27:20 +0200

    CLEAN_STYLE : contact component in landing page (not finish)

--- src/pages/landing/contact-us.jsx
+++ src/pages/landing/contact-us.jsx
@@ -16,82 +16,81 @@ const contactStyle = {
 
 export default function Contact() {
   return (
-    <div class="contact" id="contact">
-      <div class="container">
-        <span class="big-circle"></span>
-        <div class="form">
-          <div class="contact-info">
-            <h3 class="title">Let's get in touch</h3>
-            <p class="text">
-              We would love to hear from you! If you have any questions,
-              feedback, or inquiries, please don't hesitate to get in touch with
-              us. Our dedicated team is here to assist you and provide the
-              information you need.
-            </p>
+    <div class="container">
+      <div class="form">
+        <div class="contact-info">
+          <h3 class="title">Let's get in touch</h3>
+          <p class="text">
+            We would love to hear from you! If you have any questions, feedback,
+            or inquiries, please don't hesitate to get in touch with us. Our
+            dedicated team is here to assist you and provide the information you
+            need.
+          </p>
 
-            <div class="info">
-              <div class="information" style={contactStyle}>
-                <img src="/image/location.png" class="icon" alt="" />
-                <p>Cairo</p>
-              </div>
-              <div class="information" style={contactStyle}>
-                <img src="/image/email.png" class="icon" alt="" />
-                <p>MohamedAdelElbeak@gmail.com</p>
-              </div>
-              <div class="information" style={contactStyle}>
-                <img src="/image/phone.png" class="icon" alt="" />
-                <p>01553014091</p>
-              </div>
+          <div class="info">
+            <div style={contactStyle}>
+              <img src="/image/location.png" alt="location" />
+              <p>Cairo</p>
             </div>
-
-            <div class="social-media">
-              <p>Connect with us :</p>
-              <div class="social-icons">
-                <a href="#">
-                  <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
-                </a>
-                <a href="#">
-                  <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
-                </a>
-                <a href="#">
-                  <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
-                </a>
-                <a href="#">
-                  <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
-                </a>
-              </div>
+            <div style={contactStyle}>
+              <img src="/image/email.png" alt="email" />
+              <p>MohamedAdelElbeak@gmail.com</p>
+            </div>
+            <div style={contactStyle}>
+              <img src="/image/phone.png" alt="phone" />
+              <p>01553014091</p>
             </div>
           </div>
 
-          <div class="contact-form">
-            <span class="circle one"></span>
-            <span class="circle two"></span>
-
-            <form action="index.html" autocomplete="off">
-              <h3 class="title">Contact us</h3>
-              <div className="input-container">
-                <input type="text" name="name" className="input" />
-                <label for="">Username</label>
-                <span>Username</span>
-              </div>
-              <div className="input-container">
-                <input type="email" name="email" className="input" />
-                <label for="">Email</label>
-                <span>Email</span>
-              </div>
-              <div className="input-container">
-                <input type="tel" name="phone" className="input" />
-                <label for="">Phone</label>
-                <span>Phone</span>
-              </div>
-              <div className="input-container textarea">
-                <textarea name="message" className="input"></textarea>
-                <label for="">Message</label>
-                <span>Message</span>
-              </div>
-              <input type="submit" value="Send" className="btn" />
-            </form>
+          <div class="social-media">
+            <p>Connect with us :</p>
+            <div class="social-icons">
+              <a href="#">
+                <FontAwesomeIcon icon={faFacebook}></FontAwesomeIcon>
+              </a>
+              <a href="#">
+                <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+              </a>
+              <a href="#">
+                <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+              </a>
+              <a href="#">
+                <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+              </a>
+            </div>
           </div>
+        </div>
+
+        <div class="contact-form">
+          <form onSubmit={(e) => e.preventDefault()}>
+            <h3 class="title">Contact us</h3>
+            <div className="formInputs">
+              <input
+                type="text"
+                name="name"
+                className="input"
+                placeholder="Username"
+              />
+              <input
+                type="email"
+                name="email"
+                className="input"
+                placeholder="Email"
+              />
+              <input
+                type="tel"
+                name="phone"
+                className="input"
+                placeholder="Phone"
+              />
+              <textarea
+                name="message"
+                className="input"
+                placeholder="Message"
+              ></textarea>
+            </div>
+            <input type="submit" value="Send" className="btn" />
+          </form>
         </div>
       </div>
     </div>
